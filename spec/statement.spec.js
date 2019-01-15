f0265a1aaa42662badf250c8e95b42d6bddae68e
hugomenezes <Hugo_Menezes@gap.com>
2019-01-15 20:18:47 -0300

    Improving code coverage

--- spec/statement.spec.js
+++ spec/statement.spec.js
@@ -1,34 +1,33 @@
 let statement = require('../app/statement');
 
-let plays = {
-    "hamlet": { "name": "Hamlet", "type": "tragedy" },
-    "as-like": { "name": "As You Like It", "type": "comedy" },
-    "othello": { "name": "Othello", "type": "tragedy" }
-}
-
-let invoices =
-{
-    "customer": "BigCo",
-    "performances": [
-        {
-            "playID": "hamlet",
-            "audience": 55
-        },
-        {
-            "playID": "as-like",
-            "audience": 35
-        },
-        {
-            "playID": "othello",
-            "audience": 40
-        }
-    ]
-}
-
-
-describe("A statement", function () {
+describe("A statement ", function () {
 
     it("should calculate amount owed and earned points", function () {
+        let plays = {
+            "hamlet": { "name": "Hamlet", "type": "tragedy" },
+            "as-like": { "name": "As You Like It", "type": "comedy" },
+            "othello": { "name": "Othello", "type": "tragedy" }
+        }
+        
+        let invoices =
+        {
+            "customer": "BigCo",
+            "performances": [
+                {
+                    "playID": "hamlet",
+                    "audience": 55
+                },
+                {
+                    "playID": "as-like",
+                    "audience": 35
+                },
+                {
+                    "playID": "othello",
+                    "audience": 40
+                }
+            ]
+        }
+
         let calculatedStatement = statement(invoices, plays)
         expect(calculatedStatement).toBe('Statement for BigCo\n' + 
         '  Hamlet: $650.00 (55 seats)\n' +
@@ -36,6 +35,22 @@ describe("A statement", function () {
         '  Othello: $500.00 (40 seats)\n' + 
         'Amount owed is $1,730.00\n'+
         'You earned 47 credits\n');
+    });
+
+    it("should throw an error when receives a unknown play type", function () {
+        let invoices =
+        {
+            "customer": "ErrorCompany",
+            "performances": [
+                {
+                    "playID": "terror play",
+                    "audience": 666
+                }
+            ]
+        }
+        let terrorPlays = {"terror play": {"name": "terror play", "type": "terror"}}
+
+        expect(function() {statement(invoices, terrorPlays);}).toThrowError('unknown type: terror');
     });
 
 });
