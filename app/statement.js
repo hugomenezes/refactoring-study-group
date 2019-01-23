23013423322019170bc4f12d73feff3748238cc2
hugomenezes <Hugo_Menezes@gap.com>
2019-01-23 09:24:40 -0300

    JP: removed play parameter and replaced play variable by playFor

--- app/statement.js
+++ app/statement.js
@@ -9,7 +9,7 @@ function statement(invoice, plays) {
     }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -24,10 +24,10 @@ function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
 
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
 
       case "tragedy":
       result = 40000;
@@ -43,7 +43,7 @@ function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
 
     return result;
