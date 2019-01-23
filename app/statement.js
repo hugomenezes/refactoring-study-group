67b683551cf150e512187d209bf15046ae3201d6
hugomenezes <Hugo_Menezes@gap.com>
2019-01-23 09:58:58 -0300

    JP: apllying inline variable pattern to function variable format

--- app/statement.js
+++ app/statement.js
@@ -2,11 +2,6 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US",
-    {
-      style: "currency", currency: "USD",
-      minimumFractionDigits: 2
-    }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
@@ -19,8 +14,15 @@ function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
-  
-  function volumeCreditsFor(aPerformance){
+  function format(aNumber) {
+    return new Intl.NumberFormat("en-US",
+    {
+      style: "currency", currency: "USD",
+      minimumFractionDigits: 2
+    }).format(aNumber);
+  }
+
+  function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
     // add extra credit for every ten comedy attendees
@@ -28,7 +30,7 @@ function statement(invoice, plays) {
     return result;
   }
 
-
+ 
   function amountFor(aPerformance) {
     let result = 0;
 
